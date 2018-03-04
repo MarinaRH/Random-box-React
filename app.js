2c5fc0950d463d673822c2f04b1f8dbcf84340a1
MarinaRH <l_marina1717@hotmail.com>
2018-03-04 01:21:45 -0500

    Agregando ramdon fonts

--- app.js
+++ app.js
@@ -3,22 +3,30 @@ function getRandomColor() {
   var letters = '0123456789ABCDEF'.split('');
   var color = '#';
   for (var i = 0; i < 6; i++ ) {
-      color += letters[Math.round(Math.random() * 15)];
+    color += letters[Math.round(Math.random() * 15)];
   }
- 
   return color;
-  
-window.setTimeout(getRandomColor, 1000)
+}
+
+var font=new Array(
+  ['sans-serif'],
+  ['roboto'],
+  ['Helvetica'],
+  ['cursive'],
+  ['Calibri']
+);
+function getRandomFont() {
+  var index=Math.floor((Math.random()*font.length));
+  var family =font[index][0];
+  return family;
 }
 
 class Box extends React.Component {
   render(){
     return (
-      <div className="Box" style={{backgroundColor: getRandomColor()}}>
-      <p>LABORATORIA</p>
-      </div>
-      
-          
+      <div className="Box" style={{backgroundColor: getRandomColor(),color:getRandomColor(),borderColor:getRandomColor()}}>
+      <p className="Text" style={{fontFamily:getRandomFont(),fontSize:'3em'}}>LABORATORIA</p>
+      </div>    
     );
   }
 }
