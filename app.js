abaa23b777ff2069b4e5e6252dadadaa6ac540d9
MarinaRH <l_marina1717@hotmail.com>
2018-03-03 21:40:47 -0500

    agregando color ramdon

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-class Box extends React.Component {
-  render(){
-   return (
-    <div>
 
-    </div>
-   );
+function getRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var color = '#';
+  for (var i = 0; i < 6; i++ ) {
+      color += letters[Math.round(Math.random() * 15)];
   }
+ 
+  return color;
+  
+window.setTimeout(getRandomColor, 1000)
 }
 
-ReactDom.render(<Box/>,document.getElementById('app'));
+class Box extends React.Component {
+  render(){
+    return (
+      <div className="Box" style={{backgroundColor: getRandomColor()}}>
+      <p>LABORATORIA</p>
+      </div>
+      
+          
+    );
+  }
+}
+ReactDOM.render(<Box/>, document.getElementById('app'));
